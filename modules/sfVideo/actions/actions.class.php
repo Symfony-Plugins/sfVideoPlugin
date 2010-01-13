eacc2af368be828a70225dd54524c4b3e9bdf1ec
tkoomzaaskz <tkoomzaaskz@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-01-13 15:22:17 -0600

    [video] code for initial 1.0.0 release
    git-svn-id: http://svn.symfony-project.com/plugins/sfVideoPlugin/trunk@26583 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- modules/sfVideo/actions/actions.class.php
+++ modules/sfVideo/actions/actions.class.php
@@ -9,6 +9,7 @@
 
 class sfVideoActions extends sfActions
 {
+
   /**
    * Displays example links to external flv files.
    *
@@ -19,32 +20,13 @@ class sfVideoActions extends sfActions
   }
 
   /**
-   * Demo action. Displays an example external flv file.
-   *
-   * @param sfRequest $request A request object
-   */
-  public function executeShow(sfWebRequest $request)
-  {
-    // chhose one of the following examples found on the web:
-    // $this->link = "http://www.mediacollege.com/video-gallery/testclips/20051210-w50s.flv";
-    $this->link = "http://e1h13.simplecdn.net/flowplayer/flowplayer.flv";
-
-    // adding js flash embedding script
-    $this->getResponse()->addJavascript('/sfVideoPlugin/js/flowplayer-3.1.4.min.js');
-
-    // adding default flowplayer stylesheet
-    $this->getResponse()->addStylesheet('/sfVideoPlugin/css/flowplayer');
-  }
-
-  /**
    * Demo action. Displays an example local flv file.
    *
    * @param sfRequest $request A request object
    */
   public function executeLocal(sfWebRequest $request)
   {
-    $this->link = $request->getParameter('file');
-echo $this->link;
+    $this->file = $request->getParameter('file');
 
     // adding js flash embedding script
     $this->getResponse()->addJavascript('/sfVideoPlugin/js/flowplayer-3.1.4.min.js');
@@ -52,4 +34,5 @@ echo $this->link;
     // adding default flowplayer stylesheet
     $this->getResponse()->addStylesheet('/sfVideoPlugin/css/flowplayer');
   }
+
 }
